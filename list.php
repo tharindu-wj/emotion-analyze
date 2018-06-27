d36a49b5ddafaa359f6b294157fa350c284e9cc9
Tharindu <tharinduw@eight25media.com>
2018-06-27 22:00:54 +0530

    remove login

--- list.php
+++ list.php
@@ -13,7 +13,7 @@
         <!-- Example DataTables Card-->
         <div class="card mb-3">
             <div class="card-header">
-                <i class="fa fa-table"></i> Data Table Example
+                <i class="fa fa-table"></i> Captured Data List
             </div>
             <div class="card-body">
                 <div class="table-responsive">
